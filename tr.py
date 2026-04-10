9a36954c8558be4011d096fb56ad954f9dc9a5f0
Zolagos <smmerino@outlook.com>
2026-04-10 08:03:12 -0500

    prueba

---
+++ tr.py
@@ -0,0 +1,69 @@
+
+op = False
+while not op:
+    op =int(input("¿En que piso se encuentra?: (1-7)\n"))
+    if op == 1:
+        print("Usted esta en el piso mas bajo")
+        break
+    elif op == 2:
+        escalera = 2
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, es el piso mas bajo.")
+    elif op == 3:
+        escalera =3
+        print("Estoy en el piso tres, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, es el piso mas bajo.")
+    elif op == 4:
+        escalera = 4
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, es el piso mas bajo.")
+    elif op == 5:
+        escalera = 5
+        print(f"Estoy en el piso {escalera}, es el piso mas bajo.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, es el piso mas bajo.")
+    elif op == 6:
+        escalera = 6
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, es el piso mas bajo.")
+        
+    elif op == 7:
+        escalera = 7
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, el siguiente paso es bajar uno.")
+        escalera -=1
+        print(f"Estoy en el piso {escalera}, es el piso mas bajo.")
+        
